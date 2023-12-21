5d11527d8558cdcfe314403f238a083db7059193
giaruei <247658354@qq.com>
2023-12-21 17:44:18 +0800

    Feat smooth animate of sheet

--- src/components/sheet.tsx
+++ src/components/sheet.tsx
@@ -1,6 +1,7 @@
-import { ChevronRight, Heart, Award, Users2, ShieldCheck, BadgeJapaneseYen, LucideIcon } from "lucide-react"
+import { ChevronLeft, Heart, Award, Users2, ShieldCheck, BadgeJapaneseYen, LucideIcon } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import { Progress } from "@/components/ui/progress"
+import { useState } from "react"
 
 type Info = {
   label: string,
@@ -38,31 +39,44 @@ const infos: Info[] = [
 ]
 
 export const Sheet = () => {
+  const [isDisplay, setIsDisplay] = useState(false)
+
   return (
-    <div className="absolute top-0 right-0 w-[35vw] h-full flex items-center bg-white/10 backdrop-blur">
-      <Button variant="ghost" className="hover:bg-transparent">
-        <ChevronRight />
+    <div
+      className="absolute top-0 right-0 h-full flex items-center rounded-r-2xl overflow-x-hidden bg-white/10 backdrop-blur transition-all ease-in-out duration-1000"
+      style={{
+        width: isDisplay ? '35vw' : '3vw',
+        transitionDuration: isDisplay ? "700ms" : "600ms",
+        animationDuration: isDisplay ? "700ms" : "600ms"
+      }}
+    >
+      <Button onClick={() => setIsDisplay(!isDisplay)} variant="ghost" className="hover:bg-transparent">
+        <ChevronLeft className="" style={{ rotate: isDisplay ? '180deg' : '' }} />
       </Button>
-      <div className="flex flex-col">
-        <div className="w-64 px-3 py-1 mb-5 text-lg font-semibold bg-gradient-to-r from-[#3B3630]/30 to-[#5E5840]/30">CURRENT AGENT INFO</div>
-        <div className="w-[30vw] h-[30vh] p-2 bg-[#1F1F1F]">
-          <div className="w-[50%] p-1 mx-auto text-lg text-center font-semibold bg-[#5E5840]/90">李 白</div>
-          <div className="flex flex-col gap-y-2">
-            {infos.map(info => (
-              <div className="flex justify-between items-center gap-x-2">
-                <div className="flex items-center gap-x-2 w-36">
-                  <info.icon className="w-8 h-8" />
-                  <span className="text-lg font-semibold">{info.label}</span>
-                </div>
-                <Progress value={info.value} />
-                <span className=" w-6 text-lg font-semibold">{info.value}</span>
+      {
+        isDisplay && (
+          <div className="flex flex-col">
+            <div className="w-64 px-3 py-1 mb-5 text-lg font-semibold bg-gradient-to-r from-[#3B3630]/30 to-[#5E5840]/30">CURRENT AGENT INFO</div>
+            <div className="w-[30vw] h-[30vh] p-2 bg-[#1F1F1F]">
+              <div className="w-[50%] p-1 mx-auto text-lg text-center font-semibold bg-[#5E5840]/90">李 白</div>
+              <div className="flex flex-col gap-y-2">
+                {infos.map(info => (
+                  <div className="flex justify-between items-center gap-x-2">
+                    <div className="flex items-center gap-x-2 w-36">
+                      <info.icon className="w-8 h-8" />
+                      <span className="text-lg font-semibold">{info.label}</span>
+                    </div>
+                    <Progress value={info.value} />
+                    <span className=" w-6 text-lg font-semibold">{info.value}</span>
+                  </div>
+                ))}
               </div>
-            ))}
+            </div>
+            <div className="w-52 px-3 py-1 my-5 text-lg font-semibold bg-gradient-to-r from-[#3B3630]/30 to-[#5E5840]/30">UV ANALYSIS</div>
+            <div className="w-[30vw] h-[30vh] bg-[#1F1F1F]"></div>
           </div>
-        </div>
-        <div className="w-52 px-3 py-1 my-5 text-lg font-semibold bg-gradient-to-r from-[#3B3630]/30 to-[#5E5840]/30">UV ANALYSIS</div>
-        <div className="w-[30vw] h-[30vh] bg-[#1F1F1F]"></div>
-      </div>
+        )
+      }
     </div>
   )
 }
--- src/pages/ue/index.tsx
+++ src/pages/ue/index.tsx
@@ -3,9 +3,9 @@ import { Button } from "@/components/ui/button";
 
 export default function UEPage() {
   return (
-    <div className="flex flex-col justify-center items-center gap-y-3 p-1">
+    <div className="flex flex-col justify-center items-center gap-y-3 p-1 overflow-x-hidden">
       <div className="text-white text-2xl font-semibold">点击esc退出实景操控</div>
-      <div className="relative w-[85vw] h-[85vh] bg-no-repeat bg-center bg-cover" style={{ backgroundImage: `url("/ue.png")` }}>
+      <div className="relative w-[85vw] h-[85vh] rounded-2xl bg-no-repeat bg-center bg-cover" style={{ backgroundImage: `url("/ue.png")` }}>
         <Sheet></Sheet>
       </div>
       <Button className="w-44 h-14 text-lg font-semibold">返回首页</Button>
