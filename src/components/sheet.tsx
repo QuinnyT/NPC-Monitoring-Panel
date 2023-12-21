e187be66053c892851ccc82dc8ca1c19e28f68d9
giaruei <247658354@qq.com>
2023-12-21 15:52:13 +0800

    Refactor agent info card

--- src/components/sheet.tsx
+++ src/components/sheet.tsx
@@ -1,7 +1,41 @@
-import { ChevronRight, Heart, Award, Users2, ShieldCheck, BadgeJapaneseYen } from "lucide-react"
+import { ChevronRight, Heart, Award, Users2, ShieldCheck, BadgeJapaneseYen, LucideIcon } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import { Progress } from "@/components/ui/progress"
 
+type Info = {
+  label: string,
+  icon: LucideIcon,
+  value: number
+}
+
+const infos: Info[] = [
+  // todo: get & change value from...
+  {
+    label: 'Honor',
+    icon: Award,
+    value: 2
+  },
+  {
+    label: 'Intimacy',
+    icon: Heart,
+    value: 10
+  },
+  {
+    label: 'Social',
+    icon: Users2,
+    value: 40
+  },
+  {
+    label: 'Belonging',
+    icon: ShieldCheck,
+    value: 78
+  },
+  {
+    label: "Survival",
+    icon: BadgeJapaneseYen,
+    value: 52
+  }
+]
 
 export const Sheet = () => {
   return (
@@ -14,46 +48,16 @@ export const Sheet = () => {
         <div className="w-[30vw] h-[30vh] p-2 bg-[#1F1F1F]">
           <div className="w-[50%] p-1 mx-auto text-lg text-center font-semibold bg-[#5E5840]/90">李 白</div>
           <div className="flex flex-col gap-y-2">
-            <div className="flex justify-between items-center gap-x-2">
-              <div className="flex items-center gap-x-2 w-36">
-                <Award className="w-8 h-8" />
-                <span className="text-lg font-semibold">Honor</span>
+            {infos.map(info => (
+              <div className="flex justify-between items-center gap-x-2">
+                <div className="flex items-center gap-x-2 w-36">
+                  <info.icon className="w-8 h-8" />
+                  <span className="text-lg font-semibold">{info.label}</span>
+                </div>
+                <Progress value={info.value} />
+                <span className=" w-6 text-lg font-semibold">{info.value}</span>
               </div>
-              <Progress value={35} />
-              <span className="text-lg font-semibold">35</span>
-            </div>
-            <div className="flex justify-between items-center gap-x-2">
-              <div className="flex items-center gap-x-2 w-36">
-                <Heart className="w-8 h-8" />
-                <span className="text-lg font-semibold">Intimacy</span>
-              </div>
-              <Progress value={0} />
-              <span className="text-lg font-semibold">10</span>
-            </div>
-            <div className="flex justify-between items-center gap-x-2">
-              <div className="flex items-center gap-x-2 w-36">
-                <Users2 className="w-8 h-8" />
-                <span className="text-lg font-semibold">Social</span>
-              </div>
-              <Progress value={10} />
-              <span className="text-lg font-semibold">10</span>
-            </div>
-            <div className="flex justify-between items-center gap-x-2">
-              <div className="flex items-center gap-x-2 w-36">
-                <ShieldCheck className="w-8 h-8" />
-                <span className="text-lg font-semibold">Belonging</span>
-              </div>
-              <Progress value={80} />
-              <span className="text-lg font-semibold">80</span>
-            </div>
-            <div className="flex justify-between items-center gap-x-1">
-              <div className="flex items-center gap-x-2 w-36">
-                <BadgeJapaneseYen className="w-8 h-8" />
-                <span className="text-lg font-semibold">Survival</span>
-              </div>
-              <Progress value={60} />
-              <span className="text-lg font-semibold">60</span>
-            </div>
+            ))}
           </div>
         </div>
         <div className="w-52 px-3 py-1 my-5 text-lg font-semibold bg-gradient-to-r from-[#3B3630]/30 to-[#5E5840]/30">UV ANALYSIS</div>
