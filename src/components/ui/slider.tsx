ec98203fc472469f0321214232f8c9774c00c286
mengye <mengye@chaocanshu.ai>
2024-01-17 10:23:04 +0800

    feature: 添加玫瑰图timeline拖动

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -6,16 +6,17 @@ import { cn } from "@/lib/utils"
 const Slider = React.forwardRef<
     React.ElementRef<typeof SliderPrimitive.Root>,
     React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
->(({ className, value, defaultValue, step, ...props }) => (
+>(({ className, value, defaultValue, step, ...props }, ref) => (
     <SliderPrimitive.Root
-        className={cn("w-36 h-1 select-none flex items-center relative", className)}
+        className={cn("w-44 h-1 select-none flex items-center relative", className)}
         defaultValue={defaultValue}
         orientation="horizontal"
         value={value}
         step={step}
         {...props}
+        ref={ref}
     >
-        <SliderPrimitive.Track className="block relative rounded-full h-full w-full">
+        <SliderPrimitive.Track className="block relative rounded-full h-full w-full bg-[#636363]">
             <SliderPrimitive.Range className="block absolute bg-white rounded-full h-full" />
         </SliderPrimitive.Track>
         <SliderPrimitive.Thumb className="block w-4 h-4 bg-white rounded-full outline-none" />
