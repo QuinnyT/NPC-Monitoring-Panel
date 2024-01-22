e4e50681788266c5daa356af1d1304a9fb375a68
giaruei <giarueicai@chaocanshu.ai>
2024-01-22 15:54:28 +0800

    Add 'Maslow's Hierarchy of Needs' in hovor card

--- src/pages/ue/components/sheet.tsx
+++ src/pages/ue/components/sheet.tsx
@@ -75,84 +75,96 @@ export const Sheet = () => {
   }, [redisData]);
 
   return (
-    <div
-      onClick={(e) => {
-        e.preventDefault();
-        e.stopPropagation();
-      }}
-      className="absolute top-0 z-40 right-11 h-full flex items-center rounded-r-2xl overflow-x-hidden bg-[#6F6F6FCC]/50 backdrop-blur transition-all ease-in-out"
-      style={{
-        width: isDisplay ? "25vw" : "3vw",
-        transitionDuration: isDisplay ? "700ms" : "600ms",
-        animationDuration: isDisplay ? "700ms" : "600ms",
-      }}
-    >
-      <Button
-        onClick={() => setIsDisplay(!isDisplay)}
-        variant="ghost"
-        className="-mt-16 hover:bg-transparent"
+    <div>
+      <div
+        onClick={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+        }}
+        className="absolute z-40 top-0 right-11 h-full flex items-center rounded-r-2xl overflow-x-hidden bg-[#6F6F6FCC]/50 backdrop-blur transition-all ease-in-out"
+        style={{
+          width: isDisplay ? "25vw" : "3vw",
+          transitionDuration: isDisplay ? "700ms" : "600ms",
+          animationDuration: isDisplay ? "700ms" : "600ms",
+        }}
       >
-        <ChevronLeft
-          className="transition-all duration-500 text-white"
-          style={{ rotate: isDisplay ? "180deg" : "" }}
-        />
-      </Button>
-      {isDisplay && (
-        <div className="flex flex-col h-full py-2 text-white">
-          <div className="my-4 -ml-8 text-2xl font-semibold tracking-wider">
-            CURRENT AGENT INFO
-          </div>
-          <div className="w-[23.5vw] h-[28vh] -ml-10 p-2 px-3 bg-[#1F1F1FB2] rounded-3xl">
-            <div className="flex justify-between items-center mb-2">
-              <div
-                className="my-2 pl-4 text-2xl font-semibold"
-                style={{ letterSpacing: "0.625rem" }}
-              >
-                李白
-              </div>
-
-              <HoverCard>
-                <HoverCardTrigger>
-                  <HelpCircle />
-                </HoverCardTrigger>
-                <HoverCardContent side="right" className="relative z-50">
-                  The React Framework – created and maintained by @vercel.
-                </HoverCardContent>
-              </HoverCard>
+        <Button
+          onClick={() => setIsDisplay(!isDisplay)}
+          variant="ghost"
+          className="-mt-10 hover:bg-transparent"
+        >
+          <ChevronLeft
+            className="transition-all duration-500 text-white"
+            style={{ rotate: isDisplay ? "180deg" : "" }}
+          />
+        </Button>
+        {isDisplay && (
+          <div className="flex flex-col h-full py-2 text-white">
+            <div className="my-4 -ml-8 text-2xl font-semibold tracking-wider">
+              CURRENT AGENT INFO
             </div>
-            {/* <div className="w-[50%] p-1 mx-auto text-lg text-center font-semibold bg-[#5E5840]/90">{redisData.length ? redisData[0].name : ""}</div> */}
-            <div className="flex flex-col gap-y-3 pl-3">
-              {infos.map((info) => (
-                <div key={info.label} className="flex items-center gap-x-4">
-                  <div className="flex items-center gap-x-2 w-36">
-                    <info.icon className="w-6 h-6" />
-                    <span className="text-md font-semibold">{info.label}</span>
-                  </div>
-                  <Progress
-                    value={attrValues![info.label]}
-                    className="w-40 transition-all duration-200"
-                  />
-                  <span className="w-6 text-md font-semibold">
-                    {attrValues![info.label]}
-                  </span>
+            <div className="w-[23.5vw] h-[28vh] -ml-10 p-2 px-3 bg-[#1F1F1FB2] rounded-3xl">
+              <div className="flex justify-between items-center mb-2">
+                <div
+                  className="my-2 pl-4 text-2xl font-semibold"
+                  style={{ letterSpacing: "0.625rem" }}
+                >
+                  李白
                 </div>
-              ))}
+              </div>
+              {/* <div className="w-[50%] p-1 mx-auto text-lg text-center font-semibold bg-[#5E5840]/90">{redisData.length ? redisData[0].name : ""}</div> */}
+              <div className="flex flex-col gap-y-3 pl-3">
+                {infos.map((info) => (
+                  <div key={info.label} className="flex items-center gap-x-4">
+                    <div className="flex items-center gap-x-2 w-36">
+                      <info.icon className="w-6 h-6" />
+                      <span className="text-md font-semibold">
+                        {info.label}
+                      </span>
+                    </div>
+                    <Progress
+                      value={attrValues![info.label]}
+                      className="w-36 transition-all duration-200"
+                    />
+                    <span className="w-6 text-md font-semibold">
+                      {attrValues![info.label]}
+                    </span>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className="mt-16 mb-4 -ml-8 text-2xl font-semibold tracking-wider">
+              DATA ANALYSIS
+            </div>
+            <div className="relative w-[23.5vw] h-[36vh] -ml-10 px-2 flex justify-between items-center bg-[#1F1F1FB2] rounded-3xl">
+              <div className=" h-[32vh] ml-2 flex flex-col items-center gap-y-3">
+                <p className="text-xl font-semibold">Equilibrium State</p>
+                <UVBar UV={UV} />
+              </div>
+              <div className=" h-[32vh] flex flex-col items-center gap-y-3">
+                <p className="text-xl font-semibold">Energy Scale</p>
+                <RoseGraph isDisplay />
+              </div>
             </div>
           </div>
-          <div className="mt-16 mb-4 -ml-8 text-2xl font-semibold tracking-wider">
-            DATA ANALYSIS
-          </div>
-          <div className="relative w-[23.5vw] h-[36vh] -ml-10 px-2 flex justify-between items-center bg-[#1F1F1FB2] rounded-3xl">
-            <div className=" h-[32vh] ml-2 flex flex-col items-center gap-y-3">
-              <p className="text-xl font-semibold">Equilibrium State</p>
-              <UVBar UV={UV} />
-            </div>
-            <div className=" h-[32vh] flex flex-col items-center gap-y-3">
-              <p className="text-xl font-semibold">Energy Scale</p>
-              <RoseGraph isDisplay />
-            </div>
-          </div>
-        </div>
+        )}
+      </div>
+      {isDisplay && (
+        <HoverCard openDelay={50}>
+          <HoverCardTrigger asChild className="absolute z-50 top-24 right-20">
+            <HelpCircle />
+          </HoverCardTrigger>
+          <HoverCardContent
+            side="right"
+            className="relative top-20 bg-[#D9D9D9]/90 w-48 h-72"
+          >
+            <img
+              className="absolute top-9 right-4 object-cover"
+              src="/UI_triangle.png"
+              alt="Maslow"
+            />
+          </HoverCardContent>
+        </HoverCard>
       )}
     </div>
   );
