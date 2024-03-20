0cde828ddf35ed0882bf840a7f56dd181d2ef826
quinnytang <quinnytang@chaocanshu.ai>
2024-03-20 17:15:46 +0800

    添加事件模块（UI部分）

---
+++ src/components/ui/dropdownmenu.tsx
@@ -0,0 +1,75 @@
+import * as React from "react";
+import * as DropdownMenuPrimitive from "@radix-ui/react-dropdown-menu";
+
+import { cn } from "@/lib/utils"
+
+const DropdownMenu = React.forwardRef<
+  React.ElementRef<typeof DropdownMenuPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Root>
+>(({ ...props }) => (
+  <DropdownMenuPrimitive.Root
+    {...props}
+  />
+))
+DropdownMenu.displayName = DropdownMenuPrimitive.Root.displayName
+
+const DropdownMenuTrigger = React.forwardRef<
+  React.ElementRef<typeof DropdownMenuPrimitive.Trigger>,
+  React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Trigger>
+>(({ className, ...props }, ref) => (
+  <DropdownMenuPrimitive.Trigger
+  ref={ref}
+  className={cn(
+    className,
+    "flex justify-center items-center"
+  )}
+  {...props}
+  />
+))
+DropdownMenuTrigger.displayName = DropdownMenuPrimitive.Trigger.displayName
+
+const DropdownMenuPortal = React.forwardRef<
+  React.ElementRef<typeof DropdownMenuPrimitive.Portal>,
+  React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Portal>
+>(({ ...props }) => (
+  <DropdownMenuPrimitive.Portal
+    {...props}
+  />
+))
+DropdownMenuPortal.displayName = DropdownMenuPrimitive.Portal.displayName
+
+const DropdownMenuContent = React.forwardRef<
+  React.ElementRef<typeof DropdownMenuPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Content>
+>(({ className, ...props }, ref) => (
+  <DropdownMenuPrimitive.Content
+  ref={ref}
+  className={cn(
+    "z-[99]",
+    className
+  )}
+  {...props}
+  // sideOffset={5}
+  />
+))
+DropdownMenuContent.displayName = DropdownMenuPrimitive.Content.displayName
+
+const DropdownMenuItem = React.forwardRef<
+  React.ElementRef<typeof DropdownMenuPrimitive.Item>,
+  React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Item>
+>(({ className, ...props }, ref) => (
+  <DropdownMenuPrimitive.Item
+  ref={ref}
+  className={cn(
+    "flex justify-center hover:bg-black hover:text-white",
+    className
+  )}
+  {...props}
+  />
+))
+DropdownMenuItem.displayName = DropdownMenuPrimitive.Item.displayName
+
+
+
+
+export { DropdownMenu, DropdownMenuTrigger, DropdownMenuPortal, DropdownMenuContent, DropdownMenuItem };
--- src/pages/ue/components/sheet.tsx
+++ src/pages/ue/components/sheet.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState, useRef } from "react";
 
 import { Button } from "@/components/ui/button";
 import { Progress } from "@/components/ui/progress";
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuPortal, DropdownMenuContent, DropdownMenuItem } from "@/components/ui/dropdownmenu";
+
 import {
   HoverCard,
   HoverCardContent,
@@ -21,6 +23,7 @@ import {
 import { AttrValue, useRedis } from "@/hooks/use-redis";
 import RoseGraph from "./rose-graph";
 import { UVBar } from "./uv-bar";
+// import { DropdownMenuPortal } from "@radix-ui/react-dropdown-menu";
 
 type Info = {
   label: string;
@@ -49,6 +52,39 @@ const infos: Info[] = [
     icon: BadgeJapaneseYen,
   },
 ];
+
+type HistoryEvent = {
+  frame: string;
+  name: string;
+};
+
+const historyevent: HistoryEvent[] = [
+  {
+    frame: "10",
+    name: "Test1",
+  },
+  {
+    frame: "100",
+    name: "Test2",
+  },
+  {
+    frame: "20",
+    name: "Test3",
+  },
+  {
+    frame: "99",
+    name: "Test4",
+  },
+  {
+    frame: "111",
+    name: "Test5",
+  },
+  {
+    frame: "151",
+    name: "Test6",
+  },
+];
+
 
 export const Sheet = () => {
   const { redisData } = useRedis();
@@ -82,6 +118,7 @@ export const Sheet = () => {
     console.log(box.current);
     if (!box.current) return
 
+    // eslint-disable-next-line no-undef
     const ref = box.current as React.HTMLAttributes<HTMLDivElement>
     if (open) {
       ref.style!.overflow = "visible";
@@ -117,10 +154,79 @@ export const Sheet = () => {
         </Button>
         {isDisplay && (
           <div className="flex flex-col h-full py-2 text-white">
-            <div className="my-4 -ml-8 text-2xl font-semibold tracking-wider">
+            <div className="my-4 -ml-8 text-2xl font-semibold tracking-wider flex">
               CURRENT AGENT INFO
             </div>
-            <div className="w-[23.5vw] h-[28vh] relative -ml-10 p-2 px-3 bg-[#1F1F1FB2] rounded-3xl">
+            
+            <div className="flex w-[23.5vw] h-[28vh]  -ml-10 p-2 px-3 bg-[#1F1F1FB2] rounded-3xl">
+              <div className="w-[10vw] h-full relative">
+                <HoverCard openDelay={50} onOpenChange={handleHoverCardOpenChange}>
+                <HoverCardTrigger asChild className="absolute top-6 right-10">
+                  <HelpCircle />
+                </HoverCardTrigger>
+                <HoverCardContent
+                  side="right"
+                  className="relative top-20 bg-[#D9D9D9]/90 w-48 h-72"
+                >
+                  <img
+                    className="absolute top-9 right-4 object-cover"
+                    src="/UI_triangle.png"
+                    alt="Maslow"
+                  />
+                </HoverCardContent>
+                </HoverCard>
+                <div className="flex justify-between items-center mb-2">
+                <div
+                  className="my-2 pl-4 text-2xl font-semibold"
+                  style={{ letterSpacing: "0.625rem" }}
+                >
+                  事件
+                </div>
+                </div>
+                {/* <div className="w-[50%] p-1 mx-auto text-lg text-center font-semibold bg-[#5E5840]/90">{redisData.length ? redisData[0].name : ""}</div> */}
+                <div className="flex flex-col h-3/4 gap-y-3 pl-3 "> 
+                  <div className="flex items-center justify-between h-[10%]">
+                    <DropdownMenu>
+                      <DropdownMenuTrigger className="w-[75%] text-[0.7rem] gap-x-4 border border-[#F4F1F1] opacity-50 p-px rounded ">
+                        <img src="/UI_insert.png" />
+                        <div>选择输入事件</div>
+                        <img src="/UI_down.png" />
+                      </DropdownMenuTrigger>
+                      <DropdownMenuPortal>
+                        <DropdownMenuContent className="w-32 bg-white text-[0.7rem]">
+                          <DropdownMenuItem>
+                            <div>111</div>
+                          </DropdownMenuItem>
+                          <DropdownMenuItem>
+                            <div>222</div>
+                          </DropdownMenuItem>
+                        </DropdownMenuContent>
+                      </DropdownMenuPortal>
+                    </DropdownMenu>
+                    <Button className="w-[20%] h-full rounded text-[0.7rem] bg-[#F4F1F1]">确定</Button>
+                  </div>
+              
+                  {/* <div className="flex h-1/6 bg-white gap-x-4">
+                  </div> */}
+                  <div className="h-4/5  ">
+                    <div className="w-full h-1/5 flex items-center gap-x-4 text-md font-semibold border-y border-white ">
+                      <div className="w-[30%] pl-2">帧</div>
+                      <div className="w-[70%]">历史事件</div>
+                    </div>
+                    <div className="w-full h-[80%] gap-x-4 border-b border-white overflow-y-scroll">
+                    {historyevent.map((event) => (
+                      <div key={event.frame} className="w-full h-1/5 flex items-center gap-x-4 text-md border-b border-white border-opacity-50">
+                        <div className="w-[30%] pl-2">{event.frame}</div>
+                        <div className="w-[70%]">{event.name}</div>
+                      </div>
+                    ))}
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+
+              <div className="w-[12vw] relative ml-2">
               <HoverCard openDelay={50} onOpenChange={handleHoverCardOpenChange}>
                 <HoverCardTrigger asChild className="absolute top-6 right-10">
                   <HelpCircle />
@@ -145,7 +251,7 @@ export const Sheet = () => {
                 </div>
               </div>
               {/* <div className="w-[50%] p-1 mx-auto text-lg text-center font-semibold bg-[#5E5840]/90">{redisData.length ? redisData[0].name : ""}</div> */}
-              <div className="flex flex-col gap-y-3 pl-3">
+              <div className="flex flex-col justify-between h-3/4 gap-y-3 pl-3 ">
                 {infos.map((info) => (
                   <div key={info.label} className="flex items-center gap-x-4">
                     <div className="flex items-center gap-x-2 w-36">
@@ -158,13 +264,17 @@ export const Sheet = () => {
                       value={attrValues![info.label]}
                       className="w-36 transition-all duration-200"
                     />
-                    <span className="w-6 text-md font-semibold">
+                    <span className="w-[12vw] text-md font-semibold">
                       {attrValues![info.label]}
                     </span>
                   </div>
                 ))}
               </div>
+              </div>
             </div>
+            
+
+
             <div className="mt-16 mb-4 -ml-8 text-2xl font-semibold tracking-wider">
               DATA ANALYSIS
             </div>
